2600242638eb899c424dd395a67cffa63e45ba26
Shauli Ravfogel <shauli321@gmail.com>
2024-04-14 22:49:47 +0300

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -2,10 +2,12 @@ from transformers.generation import LogitsProcessor,LogitsProcessorList
 import numpy as np
 
 class GumbelProcessor(LogitsProcessor):
-    def __init__(self):
-        pass
+    def __init__(self, precomputed_noise=None):
+        self.precomputed_noise = precomputed_noise
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor):
+        if self.precomputed_noise is not None:
+            return scores + self.precomputed_noise
         gumbel = np.random.gumbel(loc=0.0, scale=1.0, size=scores.shape)
         return scores + gumbel
 
