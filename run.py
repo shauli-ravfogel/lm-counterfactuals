019a763061791c5eae9ec7de4d4c9d6423393850
Shauli Ravfogel <shauli321@gmail.com>
2024-10-01 20:05:28 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,134 +1,71 @@
-from datasets import load_dataset
-from transformers.generation.logits_process import LogitsProcessor,LogitsProcessorList
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
-import numpy as np
-from scipy.stats import gumbel_l
-#from arsenal.maths.rvs import TruncatedDistribution
-import copy
-#import os
-#os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
-import torch
-import tqdm
-import scipy
-
-from scipy.stats import gumbel_l, gumbel_r
-#from arsenal.maths.rvs import TruncatedDistribution
+import utils
+import argparse
+from mimic import InterventionModule, insert_intervention, insert_intervention
+from transformers import AutoTokenizer
 import transformers
-from evaluate import load
+import tqdm
 import pickle
 
+if __name__ == "__main__":
 
-from collections import Counter, defaultdict
-import ot
-import sk2torch
-from sklearn.linear_model import SGDClassifier
-# import nn
-import torch.nn as nn
-import copy
-import pyreft
-from sampling import generate_with_logits, counterfactual_generation_vectorized2
-import tqdm
-import os
-from torch import Tensor
-from transformer_lens import HookedTransformer, utils
-from transformer_lens.hook_points import HookPoint
-import pyvene as pv
-from transformers import pipeline
-from sampling import GumbelProcessor
-
-os.environ['HF_HOME'] = "/cluster/scratch/sravfogel/hf"
-
-def direction_ablation_hook(
-    activation,
-    hook,
-    direction
-):
-    proj = einops.einsum(activation, direction.view(-1, 1), '... d_act, d_act single -> ... single') * direction
-    return activation - proj
+    parser = argparse.ArgumentParser(description="")   #parser is an object of the class Argument Parser.
+    parser.add_argument("--dataset_name", type=str, default="sentence-transformers/wikipedia-en-sentences", required=False)
+    #parser.add_argument("--dataset_name", type=str, default="bios", required=False)
+    parser.add_argument("--bios_zs_to_keep", type=list, default=[1], required=False)
+    parser.add_argument("--bios_ys_to_keep", type=list, default=["professor"], required=False)
+    parser.add_argument("--num_sents", type=int, default=500,required=False)
+    parser.add_argument("--prompt", type=str, default="first_k",required=False)
+    parser.add_argument("--prompt_first_k", type=int, default=5,required=False)
+    parser.add_argument("--max_new_tokens", type=int, default=25,required=False)
+    parser.add_argument("--models", type=list, default=[
+         #("openai-community/gpt2-xl", "mimic_gender_gpt2_instruct"),
+         #("meta-llama/Meta-Llama-3-8B-Instruct", "mimic_gender_llama3_instruct"),
+         #("meta-llama/Meta-Llama-3-8B-Instruct", "honest_steering_llama3_instruct"),
+         #("meta-llama/Meta-Llama-3-8B", "chat_llama3"),
+         #("openai-community/gpt2-xl", "rome_louvre_gpt2_xl"),
+         #("openai-community/gpt2-xl", "GPT2-memit-louvre-rome"),
+         ("openai-community/gpt2-xl", "GPT2-memit-koalas-new_zealand")
+            ], required=False)
+    args = parser.parse_args()    
+    # load data
     
-if __name__ == '__main__':
-    ds = load_dataset("sentence-transformers/wikipedia-en-sentences")
-    num_sents = 500
-    models = [  ("meta-llama/Meta-Llama-3-8B-Instruct", "jujipotle/honest_llama3_8B_instruct"),
-              ("openai-community/gpt2-xl", "jas-ho/rome-edits-louvre-rome"),
-             ("meta-llama/Llama-2-7b-hf", "meta-llama/Llama-2-7b-chat-hf"),
-            ]
+    bios_args = {"zs_to_keep": args.bios_zs_to_keep, "ys_to_keep": args.bios_ys_to_keep}
+    sents = utils.load_sents_dataset(args.dataset_name, bios_args)[:args.num_sents]
     
-    add_prompt=True
-    sents = ds["train"]["sentence"][:num_sents]
-    device1,device2 = "cuda:0", "cuda:1"
-
-    fwd_hooks = None
-    for orig, counter in models: 
-        original_model = transformers.AutoModelForCausalLM.from_pretrained(
-            orig, device_map="auto", torch_dtype=torch.float32,trust_remote_code=True)
-        counterfactual_model = transformers.AutoModelForCausalLM.from_pretrained(
-                counter if (not 'intervenable' in counter and not "refusal" in counter) else orig, device_map="auto",torch_dtype=torch.float32,trust_remote_code=True)
-        if 'intervenable' in counter:
-            pv_model = pv.IntervenableModel.load(
-            counter, 
-            counterfactual_model,
-        )
-
-        if "refusal" in counter:
-            refusal_dir = torch.load("directions/direction_qwen_refusal.pt").to(counterfactual_model.device)
-            N_INST_TEST = 32
-            intervention_dir = refusal_dir
-            intervention_layers = list(range(counterfactual_model.cfg.n_layers)) # all layers
-            hook_fn = functools.partial(direction_ablation_hook,direction=intervention_dir)
-            fwd_hooks = [(utils.get_act_name(act_name, l), hook_fn) for l in intervention_layers for act_name in ['resid_pre', 'resid_mid', 'resid_post']]
-
-        for param in original_model.parameters():
-            param.data = param.data.to(torch.float32)
-        for param in counterfactual_model.parameters():
-            param.data = param.data.to(torch.float32)
-
-
-        tokenizer = transformers.AutoTokenizer.from_pretrained(
-            orig, model_max_length=512, 
-            padding_side="right", use_fast=False,trust_remote_code=True)
+    
+    for (orig, intervention_type) in args.models: 
+        utils.set_seed(0)
+        
+        # load model
+        
+        original_model = utils.load_model(orig)
+        counterfactual_model = utils.get_counterfactual_model(intervention_type)
+        tokenizer = transformers.AutoTokenizer.from_pretrained(orig, model_max_length=512, padding_side="right", use_fast=False,trust_remote_code=True)
         if tokenizer.bos_token is None:
             tokenizer.bos_token = tokenizer.eos_token
-            
-        vocab_size = len(tokenizer.get_vocab())
-
-        prompt = tokenizer.bos_token
-        if add_prompt:
-            prompt = tokenizer.bos_token + "{}"
-        conts = []
-
-        generation_config = GenerationConfig(
-            token_healing=True,
-            pad_token_id=tokenizer.eos_token_id,
-            eos_token_id=tokenizer.eos_token_id,
-            bos_token_id = tokenizer.bos_token_id,
-            do_sample=False,
-            num_beams=1,
-            max_new_tokens=30
-        )
-
-        all_sents = []
+        
+        all_sents, all_outputs = [], []
         for sentence in tqdm.tqdm(sents):
-            original_continuation = " ".join(sentence.split(" ")[5:])
-            full_prompt = prompt if not add_prompt else prompt.format(" ".join(sentence.split(" ")[:5]))
-            tokens_prompt = tokenizer.encode(full_prompt, return_tensors="pt", add_special_tokens=False).to(counterfactual_model.device)
-            text_generated_orig = original_model.generate(tokens_prompt, do_sample=True, max_new_tokens=20)
 
-            text_generated_orig = text_generated_orig[:,tokens_prompt.shape[1]:]
-            text_generated_orig = tokenizer.decode(text_generated_orig.detach().cpu().numpy()[0], skip_special_tokens=True)
-            original_continuation = text_generated_orig
+            # construct prompt
             
-            noise, ind2noise,logits = counterfactual_generation_vectorized2(original_model, tokenizer, full_prompt,original_continuation, vocab_size)
-            #out, cont = generate_with_logits(counterfactual_model, tokenizer, full_prompt, max_new_tokens=30, noise=noise,
-            #                                            first_idx = None,fwd_hooks=fwd_hooks)
-            processor = GumbelProcessor(torch.tensor(noise).to(original_model.device))
-            text = counterfactual_model.generate(tokens_prompt, logits_processor=[processor], generation_config=generation_config)
-            text = tokenizer.decode(text.detach().cpu().numpy()[0], skip_special_tokens=True)
-            conts.append(text)
-            print("Generated countefactual: \n\t\t{} For sentence:\n\t\t{}".format(text,full_prompt + "" + original_continuation))
-            all_sents.append(full_prompt.replace(tokenizer.bos_token,"")+original_continuation)
-            #counterfactual_model.to(device2)
-            #original_model.to(device1)
+            if args.prompt == "first_k":
+                prompt = tokenizer.bos_token + " ".join(sentence.split(" ")[:args.prompt_first_k])
+            else:
+                prompt = tokenzier.bos_token
+
+            # get counterfactual
             
-            with open(f"{orig.split("/")[1]}_{counter.split("/")[1]}_prompt:{add_prompt}.pkl", "wb") as f:
-                pickle.dump({"original": all_sents, "counter": conts}, f)
+            original_continuation_tokens, original_continuation = utils.get_continuation(original_model, tokenizer, prompt, max_new_tokens=args.max_new_tokens, return_only_continuation=True,num_beams=1, do_sample=True, token_healing=True)
+            count_tokens, count_text = utils.get_counterfactual_output(counterfactual_model, original_model, tokenizer, prompt, original_continuation, args.max_new_tokens)
+            all_outputs.append({"tokens": count_tokens, "text": count_text})
+            orig_str = prompt.replace(tokenizer.bos_token,"")+original_continuation
+            orig_str_tokens = tokenizer.encode(orig_str, return_tensors="pt", add_special_tokens=False).detach().cpu().numpy()[0]
+            all_sents.append({"tokens": orig_str_tokens, "text": orig_str})
+
+            print("Original: {}\n--------------------\nCounterfactual: {}".format(orig_str, count_text))
+            print("==================================")
+            dataset_name = "wiki" if "wiki" in args.dataset_name else "bios"
+            fname = f"counterfactuals2/{dataset_name}_{orig.split("/")[1]}->{intervention_type}_prompt:{args.prompt}_sents:{args.num_sents}_prompt_first_k:{args.prompt_first_k}_max_new_tokens:{args.max_new_tokens}.pkl"
+            with open(fname, "wb") as f:
+                pickle.dump({"original": all_sents, "counter": all_outputs}, f)
